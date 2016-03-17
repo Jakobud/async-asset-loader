a1fb370bd9c770b01e73ffc7f7c5bcf9193d0396
Jake Wilson <jake.wilson@otterproducts.com>
2016-03-17 17:01:17 -0600

    asset urls is now an array
    And the complete function doesn't execute until all urls in the array are loaded. Dependents also load at the same time.
    
    Signed-off-by: Jake Wilson <jake.wilson@otterproducts.com>

--- async-asset-loader.js
+++ async-asset-loader.js
@@ -4,30 +4,57 @@ var asyncAssetLoader = function(assets) {
     throw new URIError("The script " + oError.target.src + " is not accessible.");
   }
 
-  var importScript = function(sSrc, complete, assets) {
-    var oScript = document.createElement("script");
-    oScript.type = "text\/javascript";
-    oScript.async = "async";
-    oScript.onerror = loadError;
+  var importScript = function(source, onload) {
+    var script = document.createElement("script");
+    script.type = "text\/javascript";
+    script.async = "async";
+    script.onerror = loadError;
 
-    oScript.onload = function(){
-      setTimeout(function(){
-        complete();
-      });
-      setTimeout(function(){
-        loadAssets(assets);
-      });
+    if (typeof(onload) === 'function') {
+      script.onload = function(){
+        onload();
+      }
     }
-    document.body.appendChild(oScript);
-    oScript.src = sSrc;
+    document.body.appendChild(script);
+    script.src = source;
   }
 
   var loadAssets = function(assets) {
-    for (var index in assets) {
-      if (typeof(assets[index].url !== 'undefined')) {
-        importScript(assets[index].url, assets[index].complete, assets[index].deps);
-      }
-    }
+
+    // Iterate through the assets
+    assets.forEach(function(asset){
+
+      var countdown = asset.urls.length;
+
+      // Iterate through array of asset urls
+      asset.urls.forEach(function(url){
+
+        // Asynchronously load the url
+        importScript(url, function(){
+
+          // After all urls loaded...
+          if (--countdown === 0) {
+
+            // Run the complete function
+            setTimeout(function(){
+              asset.complete();
+            });
+
+            // load dependent assets
+            if (typeof(asset.deps) === 'object') {
+              setTimeout(function(){
+                loadAssets(asset.deps);
+              });
+            }
+
+          }
+
+
+        });
+
+      });
+
+    });
   }
 
   loadAssets(assets);
